94aaa56878a49caae77aa2d19e1931580c050e81
samuelvenzi <samuel.venzi@me.com>
2016-05-07 21:40:41 -0300

    PD4 almost finished

---
+++ PD4/gt-compare/include/groudtruth.hpp
@@ -0,0 +1,7 @@
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <fstream>
+
+double gtcompare(cv::Mat gt_img, cv::Mat out_img);
